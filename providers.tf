909010c93f8e3d0ffa6826ed5e39bf836e033500
URITI VENKATA NAGA KISHORE <kishoreuriti@gmail.com>
2021-09-25 21:02:47 +0530

    Create providers.tf

---
+++ providers.tf
@@ -0,0 +1,6 @@
+# We strongly recommend using the required_providers block to set the
+# Azure Provider source and version being used
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  feature {}
+}
