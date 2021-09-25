5132ea270646d5fd611458f4c79f1474b9ec6a74
URITI VENKATA NAGA KISHORE <kishoreuriti@gmail.com>
2021-09-25 22:29:48 +0530

    Update backend.tf

--- webapplication/backend.tf
+++ webapplication/backend.tf
@@ -1,4 +1,10 @@
+# We strongly recommend using the required_providers block to set the
+# Azure Provider source and version being used
 terraform {
-  backend "azurerm" {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.46.0"
+    }
   }
 }
